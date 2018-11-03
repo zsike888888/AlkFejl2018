3978582992f386486a29178222eaaee8d67b1d7f
zsike888888 <zsike888888@gmail.com>
2018-11-03 18:37:16 +0100

    teszt

--- news/src/main/data.sql
+++ news/src/main/data.sql
@@ -1,2 +1,3 @@
 INSERT INTO USER(NAME,USER_ID,PASSWORD) VALUES ('Szilagyi Erzsebet', 0001, 'jelszo');
+INSERT INTO USER(NAME,USER_ID,PASSWORD) VALUES ('Szilagyi Gabor', 0002, 'jelszo123');
 select * from USER;
