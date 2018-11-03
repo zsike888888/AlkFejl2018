47a6484ce8805af27c1773aadf60611c03ceb631
zsike888888 <zsike888888@gmail.com>
2018-11-03 18:45:06 +0100

    teszt

--- news/src/main/news/model/UserModel.java
+++ news/src/main/news/model/UserModel.java
@@ -1,4 +1,15 @@
 package main.news.model;
+//import lombok.Data;
 
+//import javax.persistence.*;
+import java.util.List;
+
+//@Entity
+//@Data
 public class UserModel {
+
+    private String name;
+   // @id
+    private Integer user_id;
+    private String password;
 }
